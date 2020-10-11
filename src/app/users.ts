b41de60d90fdcf0b82ee1f9d9511574932a32f42
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-11 22:08:34 +0300

    Make additions to the constructor.

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,18 +1,18 @@
 export class Users {
     constructor(
-        // public avatar_url: any,
-        // public bio: any,
-        // public login:any,
-        // public company: any,
-        // public location: any,
-        // public  name: any,
-        // public followers: any,
-        // public following: any,
-        // public created_at: Date,
-        // public public_repos: number,
-        // public html_url: any,
-        // public email: any,
-        // public public_gists: any,
+        public avatar_url: any,
+        public bio: any,
+        public login:any,
+        public company: any,
+        public location: any,
+        public  name: any,
+        public followers: any,
+        public following: any,
+        public created_at: Date,
+        public public_repos: number,
+        public html_url: any,
+        public email: any,
+        public public_gists: any,
     ) {
     }
 }
