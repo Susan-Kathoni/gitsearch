7dadcfcebf9c174af32e4bb8d1594573d1dc9ee5
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-10-11 22:17:07 +0300

    Add repos service and its modifications to allow users to see more information about the user's repos.

---
+++ src/app/repos.ts
@@ -0,0 +1,12 @@
+export class Repos {
+    constructor(
+        public name: string,
+        public html_url: string,
+        public description: string,
+        public update_at: string,
+        public stargazers_count: number,
+        public watchers_count: number,
+        public language: string,
+    ) {
+    }
+}
